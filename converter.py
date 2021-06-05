12c6087647209d4634745b517540a447d854e5b8
TheRealGabHas <69684024+TheRealGabHas@users.noreply.github.com>
2021-06-05 21:47:44 +0200

    Ergonomic update
    • Suppressed useless print
    • Suppressed test section
    • Comments are more understandable

--- converter.py
+++ converter.py
@@ -1,21 +1,15 @@
 from typing import List
 
-#msg = your text. It will return ASCII
+#msg = your text
+#This function returns ASCII (int)
 def encode(msg: str):
     code: List[int] = [ord(c) for c in msg]
-    print(code) 
     return code
 
 #code = your ASCII list obtained with the encode function
+#This function returns text (str)
 def decode(code: List[int]):
     decoded: List[str] = [chr(element) for element in code]
-    print(decoded)
-    m: str = ""
-    #transform decoded (List) to m (str)
-    for item in decoded:
-        m += str(item)
-    print(m)
-
-# To test the code
-#i: str = input("")
-#decode(encode(i))
+    #transform decoded (List) to f (str)
+    f: str = "".join(decoded)
+    return f
