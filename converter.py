e8709da782f6c9750946d21f6585315059c6f57a
TheRealGabHas <69684024+TheRealGabHas@users.noreply.github.com>
2022-09-16 09:15:24 +0200

    Removed the typing import
    Replaced "Typing" lib syntax annotation by the basic Python annotation.

--- converter.py
+++ converter.py
@@ -1,15 +1,13 @@
-from typing import List
-
 #msg = your text
 #This function returns ASCII as a List of int value
 def encode(msg: str):
-    code: List[int] = [ord(c) for c in msg]
+    code: list[int] = [ord(c) for c in msg]
     return code
 
 #code = your ASCII list obtained with the encode function
 #This function returns text (str)
-def decode(code: List[int]):
-    decoded: List[str] = [chr(element) for element in code]
+def decode(code: list[int]):
+    decoded: list[str] = [chr(element) for element in code]
     #transform decoded (List) to output (str)
     output: str = "".join(decoded)
     return output
